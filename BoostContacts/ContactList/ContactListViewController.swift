affd0692f1c8cce2f2c6ed57c4154ed9383eeef2
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-07-18 17:34:45 +0800

    create contact list and remove initial view controller

---
+++ BoostContacts/ContactList/ContactListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ContactListViewController.swift
+//  BoostContacts
+//
+//  Created by Faiz Mokhtar on 18/07/2020.
+//  Copyright © 2020 Faiz Mokhtar. All rights reserved.
+//
+
+import UIKit
+
+class ContactListViewController: UIViewController {
+
+    let viewModel: ContactListViewModel
+
+    // MARK: - Inits
+
+    init(viewModel: ContactListViewModel = ContactListViewModel()) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycles
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
