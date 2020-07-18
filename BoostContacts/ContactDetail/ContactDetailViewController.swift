7e344f155ee4a13edfbf2f417f7680647f741f87
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-07-18 23:38:49 +0800

    adjust text field on keyboard appear

--- BoostContacts/ContactDetail/ContactDetailViewController.swift
+++ BoostContacts/ContactDetail/ContactDetailViewController.swift
@@ -66,8 +66,7 @@ class ContactDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.leftBarButtonItem = cancelButton
-        navigationItem.rightBarButtonItem = saveButton
+        setupUI()
     }
 
     // MARK: - Actions
@@ -78,6 +77,37 @@ class ContactDetailViewController: UIViewController {
 
     @objc func didTappedSaveButton() {
         // TODO: Save contact
+    }
+
+    // MARK: - Privates
+
+    private func setupUI() {
+        navigationItem.leftBarButtonItem = cancelButton
+        navigationItem.rightBarButtonItem = saveButton
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(
+            self, selector: #selector(adjustForKeyboard),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
+        notificationCenter.addObserver(
+            self, selector: #selector(adjustForKeyboard),
+            name: UIResponder.keyboardWillChangeFrameNotification,
+            object: nil)
+    }
+
+    @objc private func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            tableView.contentInset = .zero
+        } else {
+            tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+
+        tableView.scrollIndicatorInsets = tableView.contentInset
     }
 }
 
