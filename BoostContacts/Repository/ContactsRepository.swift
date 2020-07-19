7173513bf238a2afb498f1ad730d2e36e928257b
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-07-20 00:06:18 +0800

    setup get all contacts

--- BoostContacts/Repository/ContactsRepository.swift
+++ BoostContacts/Repository/ContactsRepository.swift
@@ -17,10 +17,30 @@ protocol ContactsRepositorable {
 
 class ContactsRepository: ContactsRepositorable {
 
-    init() {}
+    private let path: URL
+    private let decoder: JSONDecoder
+
+    private var contacts: [Contact]
+
+    init(
+        contacts: [Contact] = [],
+        path: URL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0],
+        decoder: JSONDecoder = JSONDecoder()
+    ) {
+        self.contacts = contacts
+        self.path = path
+        self.decoder = decoder
+    }
 
     func getAllContacts() -> [Contact] {
-        return []
+        let filepath = path.appendingPathComponent("data.json")
+        do {
+            let data = try Data(contentsOf: filepath)
+            let contacts = try decoder.decode([Contact].self, from: data)
+            return contacts
+        } catch {
+            return []
+        }
     }
 
     func update(contact: Contact) -> Contact {
